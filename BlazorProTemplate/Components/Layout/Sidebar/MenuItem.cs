0f1a82611913074edc8cb4c6a9a0054c55e62f78
ilya Kupriyanov <ilya-kyp@yandex.ru>
2024-01-24 20:49:28 +0300

    sidebar item and data

---
+++ BlazorProTemplate/Components/Layout/Sidebar/MenuItem.cs
@@ -0,0 +1,47 @@
+﻿namespace BlazorProTemplate.Components.Layout.Sidebar;
+
+public record Suffix(string Title, string Class);
+
+
+
+public class MenuItem(string href = "#", string? title=null, string? icon = null, Suffix? suffix = null, List<MenuItem>? childMenuItems = null)
+{
+    const int _itemHeight = 50;
+
+    public string? Href { get; set; } = href;
+    public string? Title { get; set; } = title;
+    public string? Icon { get; set; } = icon;
+    public Suffix? Suffix { get; set; } = suffix;
+    public List<MenuItem>? ChildMenuItems { get; set; } = childMenuItems;
+
+    private bool _isOpened = false;
+    public bool IsOpened
+    {
+        get => _isOpened;
+        set
+        {
+            if (!HasChildMenuItems) return;
+
+            ChildMenuItems!.ForEach(x => { x.IsActive = value; if (value == false) x.IsOpened = false; });
+
+            _isOpened = value;
+        }
+    }
+
+    public bool IsActive { get; set; } = false;
+    public bool HasChildMenuItems => ChildMenuItems != null && ChildMenuItems.Count != 0;
+
+    public int CalcSubMenuHeight()
+    {
+        if (ChildMenuItems == null || !IsOpened)
+            return 0;
+
+        int sumHeight = 0;
+        foreach (var childMenuItem in ChildMenuItems)
+        {
+            sumHeight += childMenuItem.CalcSubMenuHeight();
+        }
+
+        return sumHeight + ChildMenuItems.Count * _itemHeight;
+    }
+}
---
+++ BlazorProTemplate/Components/Layout/Sidebar/SidebarData.cs
@@ -0,0 +1,63 @@
+﻿namespace BlazorProTemplate.Components.Layout.Sidebar;
+
+public class SidebarData
+{
+
+    public static List<MenuItem> GetStandardMenuItems() => new()
+    {
+        new MenuItem(href:"/home", title: "Home", icon:"ri-home-3-fill"),
+        new MenuItem(href:"/counter", title: "Counter", icon:"ri-add-box-fill"),
+        new MenuItem(href:"/weather", title: "Weather", icon:"ri-bar-chart-horizontal-line"),
+    };
+
+    public static List<MenuItem> GetGeneralMenuItems() => new()
+    {
+        new(href:"#", title:"Components", icon:"ri-vip-diamond-fill", suffix: new("Hot", "primary"), childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Grid"),
+            new MenuItem(href:"#", title:"Layout", childMenuItems:
+            [
+                new MenuItem(href:"#", title:"Forms", childMenuItems:
+                [
+                   new MenuItem(href:"#", title:"Input"),
+                   new MenuItem(href:"#", title:"Select"),
+                   new MenuItem(href:"#", title:"More", childMenuItems:
+                   [
+                       new MenuItem(href:"#", title:"CheckBox"),
+                       new MenuItem(href:"#", title:"Radio"),
+                       new MenuItem(href:"#", title:"Want more", childMenuItems:
+                       [
+                           new MenuItem(href:"#", title:"You made it"),
+                       ]),
+                   ]),
+                ]),
+            ]),
+        ]),
+
+        new MenuItem(href:"#", title:"Charts", icon:"ri-bar-chart-2-fill", childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Pie chart"),
+            new MenuItem(href:"#", title:"Line chart"),
+            new MenuItem(href:"#", title:"Bar chart"),
+        ]),
+
+        new MenuItem(title:"E-commerce", icon:"ri-shopping-cart-fill", childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Products"),
+            new MenuItem(href:"#", title:"Orders"),
+            new MenuItem(href:"#", title:"Credit card"),
+        ]),
+
+        new MenuItem(title:"Maps", icon:"ri-global-fill", childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Google maps"),
+            new MenuItem(href:"#", title:"Open street map"),
+        ]),
+        new MenuItem(title:"Theme", icon:"ri-paint-brush-fill", childMenuItems:
+        [
+            new MenuItem(href:"#", title:"Dark"),
+            new MenuItem(href:"#", title:"Light"),
+        ]),
+    };
+
+}
